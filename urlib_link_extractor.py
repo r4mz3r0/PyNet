54b0a0a4c4c10a534fa8de748b5534513b05d70b
Ramiro Gonzalez <ramirogonzalez@ocf.berkeley.edu>
2020-10-07 21:54:42 -0700

    extract_image_urllib.py

---
+++ urlib_link_extractor.py
@@ -0,0 +1,15 @@
+from urllib.request import urlopen 
+import re 
+
+def download_page(url): 
+    return urlopen(url).read().decode('utf-8') 
+def extract_links(page): 
+    link_regex = re.compile('<a[^>]+href=["\'\](.*?)["\']', re.IGNORECASE)
+    return link_regex.findall(page) 
+
+if __name__ == '__main__': 
+    target_url = 'http://www.brave.com'
+    amazon = download_page(target_url) 
+    links = extract_links(amazon)
+    for link in links: 
+        print(link)
