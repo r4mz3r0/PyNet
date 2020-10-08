8c5091907ff4ae526f32b07f85a9b0053dbe5412
Ramiro Gonzalez <ramirogonzalez@ocf.berkeley.edu>
2020-10-07 19:21:40 -0700

    Getting headers with a proxy

---
+++ get_headers.py
@@ -0,0 +1,7 @@
+import urllib.request 
+url = input("Enter the URL:")
+http_response = urllib.request.urlopen(url)
+if(http_response.code == 200):
+    print(http_response.headers)
+    for key, value in http_response.getheaders():
+        print(key,value)
