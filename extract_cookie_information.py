f81a6925e1bbc3abea273426e62681c4f9cb4dcf
Ramiro Gonzalez <ramirogonzalez@ocf.berkeley.edu>
2020-10-10 02:36:32 -0700

    fixed extract_cookie_information

--- extract_cookie_information.py
+++ extract_cookie_information.py
@@ -1,3 +1,4 @@
+from urllib import request
 import http.cookiejar 
 import urllib
 URL = 'https://github.com/'
@@ -6,9 +7,9 @@ def extract_cookie_info():
     # setup cookie jar 
     cookie_j = http.cookiejar.CookieJar() 
     # create url opener 
-    opener = urllib.requeset.build_opener(urllib.request.HTTPCookieProcessor(cookie_j)) 
+    opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cookie_j)) 
     # now access withouth any login info
-    resp = opener_open(URL) 
+    resp = opener.open(URL) 
     for cookie in cookie_j: 
         print("Cookie: %s --> %s" %(cookie.name, cookie.value)) 
     print("Headers: %s" %resp.headers) 
