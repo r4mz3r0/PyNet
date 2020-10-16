4b862d8a994faf7e84040be334d419e2b157a96a
Ramiro Gonzalez <ramirogonzalez@ocf.berkeley.edu>
2020-10-15 17:50:47 -0700

    xml

---
+++ books_iterate_xml.py
@@ -0,0 +1,13 @@
+from xml.etree.cElementTree import interparse
+
+def books(file): 
+    for event, elem in interparse(file): 
+        if event == 'start' and elem.tag == 'root': 
+            books = elem 
+        if event == 'end' and elem.tag == 'book': 
+            print('{0}, {1}, {2}, {3}, {4}'.format(elem.findtext('title'), elem.findtext('publisher'), elem.findtext('numberOfChapters'), elem.findtext('pageCount'), elem.findtext('author')))
+        if event == 'end' and elem.tag == 'chapter': 
+            print('{0}, {1}, {2}'.format(elem.findtext('chapterNumber'), elem.findtext('chapterTitle'), elem.findtext('pageCount'))) 
+
+if __name__ == '__main__': 
+    books(open("books.xml"))
---
+++ books_iterate_xmp.py
@@ -0,0 +1,1 @@
+from xml.etree.cElement
